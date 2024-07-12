2851e52b379cff0657b17956dcf7d2c7b93c91c3
Dhanushree Motanalira Somaiah <motanalirasomaiahd@cardiff.ac.uk>
2024-07-12 16:31:16 +0100

    updates

--- numberGuessing.py
+++ numberGuessing.py
@@ -1,40 +1,55 @@
 import random 
 
-print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 nad 100.")
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+
+#function to set difficulty
 level = input("Choose a difficulty. Type 'easy' or 'hard': ")
 
-if level == "easy":
-    print("You have 10 attempts remaining to guess the number")
-else:
-    attempts = 5
-    print("You have 5 attempts remaining to guess the number")
+# def set_difficulty():
+
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 nad 100.")
+
 
 
 computer_choice = random.randint(1, 100)
 guess_again = True
 
 #attempts
-
+if level == "easy":
+    turns = 10
+else:
+    turns = 5
 
 while guess_again:
-    guess = int(input("Make a guess: "))
+    for turns in range(-10):
 
-    if level == 'easy':
-        attempts = 10
-        for attempt in range(1, attempts):
-            attempts -= 1
+
+        guess = int(input("Make a guess: "))
+
+
+
+
+
         if guess > computer_choice:
             print("Too high, guess again")
-            print(f"You have {attempts} attempts remaining to guess the number")
+            turns += 1
+            print(f"You have {turns} attempts remaining to guess the number")
 
+            
         elif guess < computer_choice:
             print("Too Low, guess again")
-            print(f"You have {attempts} attempts remaining to guess the number")
+            turns += 1
+            print(f"You have {turns} attempts remaining to guess the number")
+        elif turns == 0:
+            guess_again = False
+
         else:
             print(f"You got it! The number was {computer_choice}")
             guess_again = False
 
-  
+
+ 
 
     
